61ce8e776715c287338726732c91997d200b3a66
EC2 Default User <ec2-user@ip-172-31-73-121.ec2.internal>
2023-02-16 22:12:09 +0000

    lec/lab5

---
+++ lab5.py
@@ -0,0 +1,36 @@
+'''
+lab5
+'''
+
+#3.1 
+a_color = 'green'
+if a_color=='green':
+    print('you got 5 points')
+    
+#3.2
+a_color= 'red'
+if a_color =='green':
+    print('you got 5 points')
+else: 
+    print('you got 10 points')
+
+#3.3 
+favorite_fruits= ['orange','pineapple','grape']
+if 'grape' in favorite_fruits:
+    print('you really like grape')
+if 'orange' in favorite_fruits:
+    print('you really like orange')
+if 'pineapple'in favorite_fruits:
+    print('you really like pineapple')
+    
+#3.4 
+age=90
+if age<10:
+    print('kid')
+elif age <20:
+    print('teen')
+else:
+    print('adult')
+if age > 65:
+        print('elder')
+
